89bd1d406964d79841457cf700a75cffa795fade
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-10-02 03:14:58 +1300

    package remove from azure-security-keyvault-keys/EncryptDecryptOperations

--- sdk/tools/azure-openrewrite/src/test/resources/migrationExamples/azure-security-keyvault-keys/EncryptDecryptOperations/v2/EncryptDecryptOperations.java
+++ sdk/tools/azure-openrewrite/src/test/resources/migrationExamples/azure-security-keyvault-keys/EncryptDecryptOperations/v2/EncryptDecryptOperations.java
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.v2.security.keyvault.keys.cryptography;
-
 import com.azure.v2.identity.DefaultAzureCredentialBuilder;
+import com.azure.v2.security.keyvault.keys.cryptography.CryptographyClient;
+import com.azure.v2.security.keyvault.keys.cryptography.CryptographyClientBuilder;
 import com.azure.v2.security.keyvault.keys.cryptography.models.DecryptResult;
 import com.azure.v2.security.keyvault.keys.cryptography.models.EncryptResult;
 import com.azure.v2.security.keyvault.keys.cryptography.models.EncryptionAlgorithm;
