666d8e3a6abb9e399f79435fc8af8894d94daf48
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-10-02 03:39:56 +1300

    package remove from azure-security-keyvault-secrets/HelloWorld

--- sdk/tools/azure-openrewrite/src/test/resources/migrationExamples/azure-security-keyvault-secrets/HelloWorld/v2/HelloWorld.java
+++ sdk/tools/azure-openrewrite/src/test/resources/migrationExamples/azure-security-keyvault-secrets/HelloWorld/v2/HelloWorld.java
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.v2.security.keyvault.secrets;
-
 import com.azure.v2.core.http.polling.PollResponse;
 import com.azure.v2.core.http.polling.Poller;
 import com.azure.v2.identity.DefaultAzureCredentialBuilder;
+import com.azure.v2.security.keyvault.secrets.SecretClient;
+import com.azure.v2.security.keyvault.secrets.SecretClientBuilder;
 import com.azure.v2.security.keyvault.secrets.models.DeletedSecret;
 import com.azure.v2.security.keyvault.secrets.models.KeyVaultSecret;
 import com.azure.v2.security.keyvault.secrets.models.SecretProperties;
