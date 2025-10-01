a3623a59496a75e4ec3638a69335a3bccf649e52
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-10-02 03:10:43 +1300

    package remove from azure-security-keyvault-keys/BackupAndRestoreOperations

--- sdk/tools/azure-openrewrite/src/test/resources/migrationExamples/azure-security-keyvault-keys/BackupAndRestoreOperations/v2/BackupAndRestoreOperations.java
+++ sdk/tools/azure-openrewrite/src/test/resources/migrationExamples/azure-security-keyvault-keys/BackupAndRestoreOperations/v2/BackupAndRestoreOperations.java
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.v2.security.keyvault.keys;
-
 import com.azure.v2.core.http.polling.PollResponse;
 import com.azure.v2.core.http.polling.Poller;
 import com.azure.v2.identity.DefaultAzureCredentialBuilder;
+import com.azure.v2.security.keyvault.keys.KeyClient;
+import com.azure.v2.security.keyvault.keys.KeyClientBuilder;
 import com.azure.v2.security.keyvault.keys.models.CreateRsaKeyOptions;
 import com.azure.v2.security.keyvault.keys.models.DeletedKey;
 import com.azure.v2.security.keyvault.keys.models.KeyVaultKey;
